336396faf861534db0d72a2c93e95586b6541a02
orima1995-create <orima1995@gmail.com>
2026-08-31 22:04:53 +0900

    refactor: build reusable Astro watch foundation

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://orima1995-create.github.io',
+  base: '/orima1995-creator.github.io',
+  trailingSlash: 'always',
+  output: 'static'
+});
